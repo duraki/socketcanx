196a09a990937d4d2ff30e301ab3427c9911dc40
Halis Duraki <halis_duraki@outlook.com>
2016-09-01 01:16:25 +0200

    Initial commit

---
+++ test.c
@@ -0,0 +1,6 @@
+#include "x/can/bcm.h"
+
+int main()
+{
+	return 0;
+}
---
+++ x/can/lib/types.h
@@ -0,0 +1,6 @@
+#include <stdint.h>
+#include <sys/socket.h>
+
+typedef int32_t __s32;
+typedef uint8_t  __u8;
+typedef __uint8_t sa_family_t;
