fd4b6f86607bd5e292187682d94f54c4243bd603
Halis Duraki <halis_duraki@outlook.com>
2016-09-01 09:25:50 +0200

    Added: CAN frame gw/bridge; Added: Netlink restc; Added: Raw CAN decs; Fixed: Types

--- x/can/lib/types.h
+++ x/can/lib/types.h
@@ -1,6 +1,12 @@
-#include <stdint.h>
-#include <sys/socket.h>
+#ifndef types_h
+#define types_h
 
-typedef int32_t __s32;
-typedef uint8_t  __u8;
-typedef __uint8_t sa_family_t;
+#include <stdint.h>
+
+typedef int32_t		__s32;	/* signed 32 b */
+typedef uint32_t	__u32;	/* unsigned 32 b */
+
+typedef uint8_t		__u8;	/* unsigned 8 b */
+typedef uint16_t	__u16;
+
+typedef __uint8_t 	sa_family_t;
